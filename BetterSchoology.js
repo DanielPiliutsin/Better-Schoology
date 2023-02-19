c42d6fb275cc7955c22e6970607a2220ae86aceb
Daniel Piliutsin <102699155+DanielPiliu@users.noreply.github.com>
2023-02-19 18:31:10 -0500

    Add files via upload

--- BetterSchoology.js
+++ BetterSchoology.js
@@ -1,17 +1,15 @@
 // ==UserScript==
 // @name         Better Schoology
 // @namespace    http://tampermonkey.net/
-// @version      0.1
-// @description  Improving Schoology
+// @version      Beta.1.0
+// @description  Schoology Enhancment Project
 // @author       github.com/danielpiliu
-// @match        https://manatee.schoology.com/*
+// @match        https://*.schoology.com/*
 // @icon         https://www.google.com/s2/favicons?sz=64&domain=manatee.schoology.com
 // @grant        none
 // ==/UserScript==
 
-// Check if the user is new and display a welcome message if so
 if (!localStorage.getItem("bsWelcomeMessageDismissed")) {
-  // Create the welcome message popup
   const popup = document.createElement("div");
   popup.style.position = "fixed";
   popup.style.top = "50%";
@@ -22,20 +20,14 @@ if (!localStorage.getItem("bsWelcomeMessageDismissed")) {
   popup.style.padding = "20px";
   popup.style.borderRadius = "5px";
   document.body.appendChild(popup);
-
-  // Create the popup title
   const title = document.createElement("h1");
   title.innerText = "Better Schoology Successfully Downloaded!";
   title.style.textAlign = "center";
   popup.appendChild(title);
-
-  // Create the popup version
   const version = document.createElement("p");
-  version.innerText = "Better Schoology Successfully Downloaded! Ver Beta 0.1";
+  version.innerText = "Better Schoology Successfully Downloaded! Beta 1 Edition";
   version.style.textAlign = "center";
   popup.appendChild(version);
-
-  // Create the popup OK button
   const okButton = document.createElement("button");
   okButton.innerText = "OK";
   okButton.style.marginTop = "10px";
@@ -44,19 +36,14 @@ if (!localStorage.getItem("bsWelcomeMessageDismissed")) {
   okButton.style.backgroundColor = "blue";
   okButton.style.color = "white";
   popup.appendChild(okButton);
-
-  // Add the "Don't Show Again" checkbox
   const dontShowAgain = document.createElement("input");
   dontShowAgain.setAttribute("type", "checkbox");
   dontShowAgain.style.marginLeft = "10px";
   popup.appendChild(dontShowAgain);
-
   const checkboxLabel = document.createElement("label");
   checkboxLabel.innerText = "Don't Show Again";
   checkboxLabel.style.marginLeft = "5px";
   popup.appendChild(checkboxLabel);
-
-  // Dismiss the popup and save the "Don't Show Again" setting when the OK button is clicked
   okButton.addEventListener("click", () => {
     popup.style.display = "none";
     if (dontShowAgain.checked) {
@@ -64,17 +51,11 @@ if (!localStorage.getItem("bsWelcomeMessageDismissed")) {
     }
   });
 }
-// Check if the current page is the options page
 if (window.location.pathname === "/options") {
-  // Clear the page
   document.body.innerHTML = "";
-
-  // Add the title "Options"
   const title = document.createElement("H1");
   title.innerText = "Options";
   document.body.appendChild(title);
-
-  // Add a file input for the background image
   const input = document.createElement("input");
   input.setAttribute("type", "file");
   input.setAttribute("id", "background-image");
@@ -88,8 +69,6 @@ if (window.location.pathname === "/options") {
       document.body.style.backgroundImage = `url(${e.target.result})`;
     };
   });
-
-  // Add a checkbox for disabling the overdue sidebar
 const checkbox = document.createElement("input");
 checkbox.setAttribute("type", "checkbox");
 checkbox.setAttribute("id", "disable-overdue-sidebar");
@@ -100,19 +79,15 @@ checkbox.addEventListener("change", (e) => {
 checkbox.checked = (localStorage.getItem("disableOverdueSidebar") === "true");
 document.body.appendChild(checkbox);
   document.body.appendChild(checkbox);
-
   const label = document.createElement("label");
   label.setAttribute("for", "disable-overdue-sidebar");
   label.innerText = "Disable Overdue Sidebar";
   document.body.appendChild(label);
-
-  // Set the tab title to "Options"
   document.title = "Options | Schoology";
 } else {
-  // Load the background image from local storage
 const backgroundImage = localStorage.getItem("backgroundImage");
 if (!backgroundImage) {
-  document.body.style.backgroundImage = `url("https://github.com/DanielPiliu/Better-Schoology/blob/main/DefaultBG.jpg?raw=true")`;
+  document.body.style.backgroundImage = `url("https://github.com/DanielPiliu/Better-Schoology/blob/main/Beta1BG.gif?raw=true")`;
   document.body.style.backgroundRepeat = "no-repeat";
   document.body.style.backgroundSize = "100% 100%";
   document.body.style.backgroundAttachment = "fixed";
@@ -121,17 +96,33 @@ if (!backgroundImage) {
   document.body.style.backgroundRepeat = "no-repeat";
   document.body.style.backgroundSize = "100% 100%";
   document.body.style.backgroundAttachment = "fixed";
-
-
   }
   document.querySelector("._2JX1Q._1LY8n._2SVA_._9GDcm").style.backgroundImage = 'url("https://github.com//DanielPiliu/Better-Schoology/blob/main/Logo.png?raw=true")';
-
-  // Load the status of the "disable overdue sidebar" option from local storage
   const disableOverdueSidebar = localStorage.getItem("disableOverdueSidebar");
   if (disableOverdueSidebar === "true") {
     const overdueSidebar = document.getElementById("overdue-submissions");
     if (overdueSidebar) {
       overdueSidebar.remove();
     }
+if (window.location.pathname === "/home") {
+  const clickMoreButton = () => {
+    const moreButton = document.querySelector('.active.sExtlink-processed.sEdgeMore-processed');
+    if (moreButton) {
+      moreButton.click();
+      setTimeout(clickMoreButton, 1);
+    } else {
+      setTimeout(checkIfAtBottom, 1000);
+    }
+  };
+  const checkIfAtBottom = () => {
+    const isAtBottom = (window.innerHeight + window.pageYOffset) >= document.body.offsetHeight;
+    if (isAtBottom) {
+      clickMoreButton();
+    } else {
+      setTimeout(checkIfAtBottom, 100);
+    }
+  };
+  checkIfAtBottom();
+}
   }
 }
